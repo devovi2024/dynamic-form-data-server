334713a6f0e9f4c12b82409731041969f11712df
Ovi <arfanhosenovi@gmail.com>
2024-08-27 22:39:34 +0600

    Handle form Submission

--- server.js
+++ server.js
@@ -3,3 +3,26 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const querystring  = require('querystring');
+
+
+// Handle form Submission 
+const handleFormSubmission = (req, res) => {
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+
+    req.on('end', () =>{
+        const userData = querystring.parse(body);
+
+        fs.writeFile('userData.json', JSON.stringify(userData, null, 2), (err) => {
+            if(err){
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('Error saving data')
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.end('Data saved Successfully')
+            }
+        })
+    })
+}
