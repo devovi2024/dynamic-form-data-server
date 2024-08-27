b0b34ebe2a1c3727e7ff46e865d22db3699b1c96
Ovi <arfanhosenovi@gmail.com>
2024-08-27 22:40:36 +0600

    Create an HTTP server that handles incoming request

--- server.js
+++ server.js
@@ -46,6 +46,40 @@ const handleViewUserData = (res) =>{
 
 
 
+// Create an HTTP server that handles incoming request 
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST' && req.url === '/submit') {
+      handleFormSubmission(req, res);
+    } else if (req.method === 'GET' && req.url === '/user-data') {
+      handleViewUserData(res);
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <title>User Data Form</title>
+            </head>
+            <body>
+              <h1>Enter Your Information</h1>
+              <form action="/submit" method="post">
+                <label>Name:</label><br>
+                <input type="text" name="name" required><br><br>
+                <label>Email:</label><br>
+                <input type="email" name="email" required><br><br>
+                <label>Phone:</label><br>
+                <input type="text" name="phone" required><br><br>
+                <input type="submit" value="Submit">
+              </form>
+              </br>
+               <h2>View User Data</h2>
+                <a href="/user-data">Show User Data</a>
+            </body>
+            </html>
+          `); 
+    }
+})
 
 
 
